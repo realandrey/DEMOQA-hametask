69ed09a50e159f9c0369da2d334a8bafe6448027
Andrei Barulin <andrewabarulin@gmail.com>
2024-10-13 00:43:44 +0300

    added comlete test

--- src/test/java/tests/AutomationPracticeForm.java
+++ src/test/java/tests/AutomationPracticeForm.java
@@ -1,13 +1,13 @@
 package tests;
 
-
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeForm {
 
@@ -17,21 +17,59 @@ public class AutomationPracticeForm {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        Configuration.timeout = 5000;
     }
 
     @Test
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
+        open("/automation-practice-form");
+
+        // Заполнение полей ввода
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#userEmail").setValue("john.doe@example.com");
+        $("#userNumber").setValue("1234567890");
+
+        // Выбор радиокнопки (Gender)
+        $("#gender-radio-1").parent().click();  // Male
+
+        // Установка даты рождения
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__year-select").selectOption("1995");
+        $(".react-datepicker__day--009").click();  // 9 октября
+
+        // Ввод предметов
+        $("#subjectsInput").setValue("Math").pressEnter();
+
+        // Установка чекбоксов (Hobbies)
+        $("#hobbies-checkbox-1").parent().click();  // Спорт
+        $("#hobbies-checkbox-2").parent().click();  // Чтение
+
+        // Загрузка файла
+        $("#uploadPicture").uploadFile(new File("src/test/picture.png"));
+
+        // Заполнение адреса
+        $("#currentAddress").setValue("123 Main Street");
+
+        // Выбор штата и города (Пример: NCR -> Delhi)
+        $("#state").click();
+        $x("//div[text()='NCR']").click();
+        $("#city").click();
+        $x("//div[text()='Delhi']").click();
+
+        // Отправка формы
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        // Проверка вывода
+        $(".modal-content").shouldHave(text("John Doe"),
+                text("john.doe@example.com"),
+                text("1234567890"),
+                text("Male"),
+                text("Math"),
+                text("Sports"),
+                text("Reading"),
+                text("123 Main Street"),
+                text("NCR Delhi"));
     }
 }
