34dc2b2fce9749538ea86fb554041f4c524aef99
Andrei Barulin <andrewabarulin@gmail.com>
2024-10-28 20:22:06 +0300

    change method uploadFromClasspath, make new select state and city

---
+++ src/main/java/guru/qa/TestOperators.java
@@ -0,0 +1,25 @@
+package guru.qa;
+
+public class TestOperators {
+
+//    public static void main(String... args){
+//        int a = 2147483647;
+//        int b = 1000000000;
+//                System.out.println(a + b);
+//    }
+//}
+
+
+//public static void main(String... args){
+//    double a = Double.MAX_VALUE;
+//    int b = 1000000000;
+//    System.out.println(a * b);
+//}
+//}
+
+    public static void main(String... args) {
+        double a = 23.05444444;
+        int b = 10;
+        System.out.println(a - b);
+    }
+}
