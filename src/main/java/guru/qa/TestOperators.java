5c6beb3eb2b543ba446caace1a9e9592b03b8555
Andrei Barulin <andrewabarulin@gmail.com>
2024-10-29 11:47:06 +0300

    change method uploadFromClasspath, make new select state and city

--- src/main/java/guru/qa/TestOperators.java
+++ src/main/java/guru/qa/TestOperators.java
@@ -2,24 +2,27 @@ package guru.qa;
 
 public class TestOperators {
 
-//    public static void main(String... args){
-//        int a = 2147483647;
-//        int b = 1000000000;
-//                System.out.println(a + b);
-//    }
-//}
-
-
-//public static void main(String... args){
-//    double a = Double.MAX_VALUE;
-//    int b = 1000000000;
-//    System.out.println(a * b);
-//}
-//}
-
     public static void main(String... args) {
+        performIntAddition();
+        performDoubleMultiplication();
+        performSubtraction();
+    }
+
+    private static void performIntAddition() {
+        int a = 2147483647;
+        int b = 1000000000;
+        System.out.println("Сумма int: " + (a + b));
+    }
+
+    private static void performDoubleMultiplication() {
+        double a = Double.MAX_VALUE;
+        int b = 1000000000;
+        System.out.println("Произведение double и int: " + (a * b));
+    }
+
+    private static void performSubtraction() {
         int a = 23;
         double b = 10.4444444;
-        System.out.println(a - b);
+        System.out.println("Разница int и double: " + (a - b));
     }
 }
